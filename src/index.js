2f4ff568e05370a105f855ea4272ef935328f1f9
Karol M <KarolMMM01@gmail.com>
2022-04-24 17:34:53 +0200

    Added webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import './styles/style.css'
+
 console.log('hey');
+console.log('asdasda');
+console.log('working');
+
+const addButton = document.querySelector('.title')
+const addDiv = document.querySelector('.wrapper')
+console.log(addButton);
+console.log(addDiv);
+addButton.addEventListener('click',function(){
+    addDiv.classList.add('open')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname,'src/index.js'),
   },
+  output: {
+    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer:{
+    static:{
+      directory: path.resolve(__dirname, 'src')
+    },
+    watchFiles: ["./src/*"],
+    port: 3000,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
 };
